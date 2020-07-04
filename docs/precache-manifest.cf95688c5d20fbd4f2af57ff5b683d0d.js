154c220ed749974d613ad9973b25bdcc24f69bd4
Phil Batey <philip_batey@comcast.com>
2020-07-04 01:49:44 -0600

    add demo

---
+++ docs/precache-manifest.cf95688c5d20fbd4f2af57ff5b683d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bdd66546be81e51dc7ea591f755050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caab1fa8b71994404bab",
+    "url": "/static/css/main.1938d906.chunk.css"
+  },
+  {
+    "revision": "8e7a0a47c967a798a5d7",
+    "url": "/static/js/2.54715352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54715352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caab1fa8b71994404bab",
+    "url": "/static/js/main.d1fdf1d0.chunk.js"
+  },
+  {
+    "revision": "d4fc54e5d1afe6c705e6",
+    "url": "/static/js/runtime-main.3ae98dce.js"
+  }
+]);
